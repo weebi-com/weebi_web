22200400eb0504cf0731010b6ca8e12d4dec1c52
Flutter Painter <dev@weebi.com>
2025-03-21 11:31:02 +0100

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:web_admin/root_app.dart';
 import 'package:web_admin/shared_prefs.dart';
@@ -7,10 +6,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (kReleaseMode) {
-    await dotenv.load(fileName: "dotenv_prd.txt");
-  } else {
+  const environment = String.fromEnvironment('ENVIRONMENT', defaultValue: 'production');
+  if (environment == 'development') {
     await dotenv.load(fileName: "dotenv_dev.txt");
+  } else {
+    await dotenv.load(fileName: "dotenv_prd.txt");
   }
 
   runApp(const SharedPrefsFetchWidget(child: RootApp()));
